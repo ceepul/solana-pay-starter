b7ab61f5539631a73c6c008f78fc0270d3327699
Russell Fenton <russell1837@gmail.com>
2022-11-16 19:08:00 -0500

    eddited project up until the end of the buildspace project guide

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
 import HeadComponent from '../components/Head';
+import Product from "../components/Product";
+import CreateProduct from "../components/CreateProduct";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
+// Dynamic import `WalletMultiButton` to prevent hydration error
+const WalletMultiButtonDynamic = dynamic(
+    async () =>
+      (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+      { ssr: false }
+);
+
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+
+  const isOwner = ( publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false );
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch('/api/fetchProducts')
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://variety.com/wp-content/uploads/2022/07/Music-Streaming-Wars.jpg" alt="Music Icon" width="520" />
+
+      <div className="button-container">
+        <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product)  => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
   return (
     <div className="App">
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header">Solana Music Shop</p>
+          <p className="sub-text">- All of your blockchain beats -</p>
+
+          {isOwner && (
+            <button className="create-product-button" onClick={() => setCreating(!creating)}>
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
+
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {creating && <CreateProduct />}
+
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+
+          <p>*Not selling real music*</p>
         </main>
 
         <div className="footer-container">
+          <p>This app uses the Devnet</p>
           <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
           <a
             className="footer-text"
